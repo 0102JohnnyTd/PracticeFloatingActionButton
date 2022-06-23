8b7e17f74b33e6d8b99cec3143fd227dccbd4e53
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-23 10:41:48 +0900

    XIBセルをTableViewに表示させる処理の実装

---
+++ PracticeFloatingActionButton/CustommTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustommTableViewCell.swift
+//  PracticeFloatingActionButton
+//
+//  Created by Johnny Toda on 2022/06/23.
+//
+
+import UIKit
+
+class CustommTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PracticeFloatingActionButton/ViewController.swift
+++ PracticeFloatingActionButton/ViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var customTableView: UITableView! {
+        didSet {
+            customTableView.register(UINib(nibName: cellNibName, bundle: nil), forCellReuseIdentifier: cellID)
+        }
+    }
+
+    private let cellNibName = "CustomTableViewCell"
+    private let cellID = "CellID"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        customTableView.delegate = self
+        customTableView.dataSource = self
+    }
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        customTableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! CustommTableViewCell
     }
 }
