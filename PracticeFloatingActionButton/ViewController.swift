05de520d8345c4d5cc5280109be29bd3b10fabc9
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-24 09:19:44 +0900

    FloatingActionButtonを画面右下に設置し、ボタンの高さと幅、形状をコードで定義

--- PracticeFloatingActionButton/ViewController.swift
+++ PracticeFloatingActionButton/ViewController.swift
@@ -8,26 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var customTableView: UITableView! {
+    @IBOutlet private weak var customTableView: UITableView! {
         didSet {
+            customTableView.delegate = self
+            customTableView.dataSource = self
             customTableView.register(UINib(nibName: cellNibName, bundle: nil), forCellReuseIdentifier: cellID)
         }
     }
+
+    // 画面右下に表示させるボタンを生成
+    @IBOutlet private weak var floatingActionButton: UIButton!
 
     private let cellNibName = "CustomTableViewCell"
     private let cellID = "CellID"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        customTableView.delegate = self
-        customTableView.dataSource = self
+        setUpfloatingActionButtonSize()
+    }
+
+    private func setUpfloatingActionButtonSize() {
+        // ボタンの幅と高さを指定
+        let width = UIScreen.main.bounds.width / 5
+        let height = width
+        floatingActionButton.widthAnchor.constraint(equalToConstant: width).isActive = true
+        floatingActionButton.heightAnchor.constraint(equalToConstant: height).isActive = true
+
+        // ボタンを円形に変更
+        floatingActionButton.layer.cornerRadius = width / 2
     }
 }
 
-
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        20
+        100
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
